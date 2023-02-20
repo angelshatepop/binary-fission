86875ba1324e7c828177157c1e4d48c76bf165a3
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-20 18:40:45 -0500

    0.0.1

---
+++ bit-simuli-vis-fnd/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::BTreeMap;
+mod genetic_algs;
+mod queue;
+
+fn main() {
+    let (mut alpha_xya, mut alpha_xxa) = 
+    ((vec![0,1,0,1,1,0,1,1,0],77), (vec![1,0,1,1,1,0,1,1,1],88));
+    let (mut alpha_xyb, mut alpha_xxb) = 
+    ((vec![1,1,1,0,1,1,0,1,0], 77), (vec![0,0,1,1,1,0,1,0,1],55));
+    let mut population_queue = BTreeMap::new();
+    
+    let alphachild_a = genetic_algs::crossover(&mut alpha_xya, &mut alpha_xxa);
+    let alphachild_b = genetic_algs::crossover(&mut alpha_xyb, &mut alpha_xxb);
+
+    queue::add_alphachildren(alphachild_a, alphachild_b, &mut population_queue);
+    queue::add_parents(alpha_xya, alpha_xxa, &mut population_queue);
+    queue::add_parents(alpha_xyb, alpha_xxb, &mut population_queue);
+
+    for (chromosome, fitness) in &population_queue {
+        println!("{chromosome:?}: {fitness}");
+    }
+}
