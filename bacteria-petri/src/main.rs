97d69c3de6375bc34f1ae60afa6a03b0302c86dd
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-24 14:09:01 -0500

    0.0.1...lol

--- bacteria-petri/src/main.rs
+++ bacteria-petri/src/main.rs
@@ -3,13 +3,14 @@ use std::time::Instant;
 mod cell;
 
 fn main() {
-    let mut list: Vec<cell::Cell> = Vec::new();
     let now = Instant::now();
     let log: String = "log.txt".to_string();
+    let mut list: Vec<cell::Cell> = Vec::new();
     let mut cell1 = Generate::generate("alpha".to_string(), &mut list, now, &log);
+    <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut list, now, &log);
     <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut list, now, &log);
 
     for cell in &list{
-        println!("{}", cell.name);
+        println!("{}{:?}", cell.name, cell.is_child);
     }
 }
