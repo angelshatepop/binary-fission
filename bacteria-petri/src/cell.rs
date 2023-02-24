80c5aa92091668b6ffec13d1c4551a635fcf55f7
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-24 02:53:30 -0500

    0.0.1.3

--- bacteria-petri/src/cell.rs
+++ bacteria-petri/src/cell.rs
@@ -1,33 +1,82 @@
 use rand::{Rng, distributions::Alphanumeric};
+use std::fs::{OpenOptions};
+use std::io::Write;
+use std::time::Instant;
 
 pub struct Cell { 
-    pub name: String, 
+    pub name: String,
     pub cycles: u16,
+    pub is_child: Option<bool>,
+    pub has_split: Option<bool>
 }
-pub trait Generate {
-    fn generate(vec: &mut Vec<(String, u16)>) -> Self;
+pub trait Log {
+    fn log(now: Instant, action: String, log: &String);
 }
-pub trait BinaryFission {
-    fn binary_fission(cell: &mut Cell, vec: &mut Vec<(String, u16)>);
-}
-impl Generate for Cell {
-    fn generate(vec: &mut Vec<(String, u16)>) -> Cell {
-        let name: String = rand::thread_rng().sample_iter(&Alphanumeric).take(7).map(char::from).collect();
-        let cycles: u16 = 0;
-        let cell = Cell {name, cycles};
-        vec.push((cell.name.clone(), cell.cycles.clone()));
-        cell
+impl Log for Cell {
+    fn log(now: Instant, action: String, log: &String) {
+        let mut path = OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open(log)
+                    .unwrap();
+        let elapsed_time = now.elapsed().as_secs();
+        if elapsed_time > 59{
+            writeln!(path, "[{}:{}]", elapsed_time / 60, elapsed_time%60)
+            .expect("unable to write to file");
+        }
+        else{
+            writeln!(path, "[0:{}] {}", format!("{}", elapsed_time), action)
+            .expect("unable to write to file");
+        }
     }
 }
+pub trait Data {
+    fn data(cell: &mut Cell); 
+}
+impl Data for Cell {
+    fn data(cell: &mut Cell){
+        println!("name: {}\ncycles: {}", cell.name, cell.cycles);
+    }
+}
+pub trait Generate {
+    fn generate(name: String, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Self;
+    fn generatechild(parent: &mut Cell, vec: &mut Vec<(String, u16)>) -> Self;
+}
+impl Generate for Cell {
+    fn generate(name: String, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Self {
+        let cycles = 0;
+        let cell = Cell{name, cycles, is_child: Some(false), has_split: Some(false)};
+        vec.push((cell.name.clone(), cell.cycles.clone()));
+        Cell::log(start, format!("cell: {} has been [SPAWNED]", cell.name), log);
+        cell
+    }
+    fn generatechild(parent: &mut Cell, vec: &mut Vec<(String, u16)>) -> Self {
+        let bname: String = rand::thread_rng().sample_iter(&Alphanumeric).take(7).map(char::from).collect();
+        let name = format!("{}-{}", bname, parent.name);
+        let cycles = 0;
+        let cell = Cell{name, cycles, is_child: Some(true), has_split: Some(false)};
+        vec.push((cell.name.clone(), cell.cycles.clone()));
+        return cell
+    }
+} 
+pub trait BinaryFission {
+    fn binary_fission(parent: &mut Cell, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Option<(Cell, Cell)>;
+}
 impl BinaryFission for Cell {
-    fn binary_fission(cell: &mut Cell, vec: &mut Vec<(String, u16)>) {
-        let mut i = 0;
-        loop {
-            if i < 2 {
-                let mut cell = Cell::generate(vec);
-                println!("new cell from split: {}", cell.name);
-                i += 1;
-            }
+    fn binary_fission(parent: &mut Cell, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Option<(Cell, Cell)>{
+        if parent.has_split == Some(false){
+            let (cell1, cell2) = (Cell::generatechild(parent, vec), Cell::generatechild(parent, vec));
+            let index = vec.iter().position(|(x, _)| x == &parent.name);
+            match index {
+                Some(i) => vec[i].1 += 1,
+                None => println!("{} not found in the vector", &parent.name),
+                }
+            Cell::log(start, format!("cell: {} has [SPLIT] => {} / {}", parent.name, cell1.name, cell2.name), log);
+            parent.has_split = Some(true);
+            return Some((cell1, cell2));
+        }
+        else{
+            return None;
         }
     }
 }
