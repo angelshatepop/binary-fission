c4159231f565912c103e5fb4fd4618002cdc6d8b
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-24 03:49:39 -0500

    0.0.1.3.5

--- bacteria-petri/src/cell.rs
+++ bacteria-petri/src/cell.rs
@@ -2,10 +2,9 @@ use rand::{Rng, distributions::Alphanumeric};
 use std::fs::{OpenOptions};
 use std::io::Write;
 use std::time::Instant;
-
+#[derive(Clone)]
 pub struct Cell { 
     pub name: String,
-    pub cycles: u16,
     pub is_child: Option<bool>,
     pub has_split: Option<bool>
 }
@@ -30,48 +29,38 @@ impl Log for Cell {
         }
     }
 }
-pub trait Data {
-    fn data(cell: &mut Cell); 
-}
-impl Data for Cell {
-    fn data(cell: &mut Cell){
-        println!("name: {}\ncycles: {}", cell.name, cell.cycles);
-    }
-}
+
 pub trait Generate {
-    fn generate(name: String, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Self;
-    fn generatechild(parent: &mut Cell, vec: &mut Vec<(String, u16)>) -> Self;
+    fn generate(name: String, vec: &mut Vec<Cell>, start: Instant, log: &String) -> Self;
+    fn generatechild(parent: &mut Cell, vec: &mut Vec<Cell>) -> Self;
 }
 impl Generate for Cell {
-    fn generate(name: String, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Self {
-        let cycles = 0;
-        let cell = Cell{name, cycles, is_child: Some(false), has_split: Some(false)};
-        vec.push((cell.name.clone(), cell.cycles.clone()));
-        Cell::log(start, format!("cell: {} has been [SPAWNED]", cell.name), log);
-        cell
+    fn generate(name: String, vec: &mut Vec<Cell>, start: Instant, log: &String) -> Self {
+        let cell = Cell{name, is_child: Some(false), has_split: Some(false)};
+        Cell::log(start, format!("cell: '{}' has been [SPAWNED]", cell.name), log);
+        let cell_copy = cell.clone();
+        vec.push(cell);
+
+        cell_copy
     }
-    fn generatechild(parent: &mut Cell, vec: &mut Vec<(String, u16)>) -> Self {
+    fn generatechild(parent: &mut Cell, vec: &mut Vec<Cell>) -> Self {
         let bname: String = rand::thread_rng().sample_iter(&Alphanumeric).take(7).map(char::from).collect();
         let name = format!("{}-{}", bname, parent.name);
-        let cycles = 0;
-        let cell = Cell{name, cycles, is_child: Some(true), has_split: Some(false)};
-        vec.push((cell.name.clone(), cell.cycles.clone()));
-        return cell
+        let cell = Cell{name, is_child: Some(true), has_split: Some(false)};
+
+        let cell_copy = cell.clone();
+        vec.push(cell);
+        cell_copy
     }
 } 
 pub trait BinaryFission {
-    fn binary_fission(parent: &mut Cell, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Option<(Cell, Cell)>;
+    fn binary_fission(parent: &mut Cell, vec: &mut Vec<Cell>, start: Instant, log: &String) -> Option<(Cell, Cell)>;
 }
 impl BinaryFission for Cell {
-    fn binary_fission(parent: &mut Cell, vec: &mut Vec<(String, u16)>, start: Instant, log: &String) -> Option<(Cell, Cell)>{
+    fn binary_fission(parent: &mut Cell, vec: &mut Vec<Cell>, start: Instant, log: &String) -> Option<(Cell, Cell)>{
         if parent.has_split == Some(false){
             let (cell1, cell2) = (Cell::generatechild(parent, vec), Cell::generatechild(parent, vec));
-            let index = vec.iter().position(|(x, _)| x == &parent.name);
-            match index {
-                Some(i) => vec[i].1 += 1,
-                None => println!("{} not found in the vector", &parent.name),
-                }
-            Cell::log(start, format!("cell: {} has [SPLIT] => {} / {}", parent.name, cell1.name, cell2.name), log);
+            Cell::log(start, format!("cell: '{}' has [SPLIT] => {} / {}", parent.name, cell1.name, cell2.name), log);
             parent.has_split = Some(true);
             return Some((cell1, cell2));
         }
--- bacteria-petri/src/main.rs
+++ bacteria-petri/src/main.rs
@@ -3,20 +3,13 @@ use std::time::Instant;
 mod cell;
 
 fn main() {
-    let mut list: Vec<(String, u16)> = Vec::new();
+    let mut list: Vec<cell::Cell> = Vec::new();
     let now = Instant::now();
     let log: String = "log.txt".to_string();
     let mut cell1 = Generate::generate("alpha".to_string(), &mut list, now, &log);
     <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut list, now, &log);
+
     for cell in &list{
-        println!("{:?}", cell);
-    }
-    <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut list, now, &log);
-    for cell in &list{
-        println!("{:?}", cell);
-    }
-    <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut list, now, &log);
-    for cell in &list{
-        println!("{:?}", cell);
+        println!("{}", cell.name);
     }
 }
