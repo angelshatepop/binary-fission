2293d8ac7cb90502d8c6987c0dc594600d91060f
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-27 22:10:19 -0500

    remove parent of binary fission from vec

--- simuli/src/test.rs
+++ simuli/src/test.rs
@@ -1,28 +1,25 @@
-use cell::*;
-use std::time::Instant;
-mod cell;
+// use cell::*;
+// use std::time::Instant;
+// mod cell;
+// fn main() {
+//     let now = Instant::now();
+//     let log: String = "log.txt".to_string();
+//     let mut cell_list: Vec<cell::Cell> = Vec::new();
 
-fn main() {
-    let now = Instant::now();
-    let log: String = "log.txt".to_string();
-    let mut cell_list: Vec<cell::Cell> = Vec::new();
+//     let mut alpha_cell = Generate::generate_alpha("alpha".to_string(), &mut cell_list, now, &log);
+//     let (mut cell1, mut _cell2) = <cell::Cell as BinaryFission>::binary_fission(&mut alpha_cell, &mut cell_list, now, &log).unwrap();
+    
+//     println!("\n{:?}", cell1);
+//     for cell in &cell_list{
+//         println!("{}", cell.name);
+//     }
 
-    //initialize test
-    let mut alpha_cell = Generate::generate_alpha("alpha".to_string(), &mut cell_list, now, &log);
-    let (mut cell1, mut _cell2) = <cell::Cell as BinaryFission>::binary_fission(&mut alpha_cell, &mut cell_list, now, &log).unwrap();
-    //before test
-    println!("\n{:?}", cell1);
-    for cell in &cell_list{
-        println!("{}", cell.name);
-    }
-    //test
-    let (mut _cell3, mut _cell4) = <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut cell_list, now, &log).unwrap();
-    //after test
-    println!("\n{:?}", cell1);
-    for cell in &cell_list{
-        println!("{}", cell.name);
-    }
-    //error test
-    let (mut _cell5, mut _cell6) = 
-        <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut cell_list, now, &log).unwrap();
-}
+//     let (mut _cell3, mut _cell4) = <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut cell_list, now, &log).unwrap();
+//     println!("\n{:?}", cell1);
+//     for cell in &cell_list{
+//         println!("{}", cell.name);
+//     }
+
+//     let (mut _cell5, mut _cell6) = 
+//         <cell::Cell as BinaryFission>::binary_fission(&mut cell1, &mut cell_list, now, &log).unwrap();
+// }
